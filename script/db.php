8d4dbe46a6b4716ebe45570f0f135393254ede28
darkkeks <darkkeks@rambler.ru>
2018-01-22 18:04:35 +0300

    Added heroku postgres credentials

--- script/db.php
+++ script/db.php
@@ -1,14 +1,16 @@
 <?php
 
-$_db_host = "localhost";
-$_db_user = "codepaste";
-$_db_pass = "jfhtm_7930";
-$_db_db = "codepaste";
+$_db_opts = parse_url(getenv('DATABASE_URL'));
+$_db_host = $_db_opts["host"];
+$_db_port = $_db_opts["port"];
+$_db_user = $_db_opts["user"];
+$_db_pass = $_db_opts["pass"];
+$_db_db = ltrim($_db_opts["path"],'/');
 $_db_charset = "UTF8";
 
 function DBConnect(){
-    global $_db_host, $_db_user, $_db_pass, $_db_db, $_db_charset;
-    $_db_dsn = "pgsql:host={$_db_host};port=5432;dbname={$_db_db};user={$_db_user};password={$_db_pass}";
+    global $_db_host, $_db_user, $_db_pass, $_db_db, $_db_charset, $_db_port;
+    $_db_dsn = "pgsql:host={$_db_host};port={$_db_port};dbname={$_db_db};user={$_db_user};password={$_db_pass}";
     $_db_opt = array(
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
